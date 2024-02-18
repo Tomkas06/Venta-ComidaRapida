9a912f905074f534c8e023fb1fab3e91e6fcded8
Tomkas <vecchitomas@gmail.com>
2024-02-18 01:18:35 -0300

    Cambios de diseño
    Se cambiaron y añadieron los siguientes elementos:
    1- Presentacion de las tarjetas, diseñado especialmente para su uso en celulares.
    2- Se añadio un boton que muestra la cantidad de productos añadidos.
    3- Se han cambiado las imagenes de ejemplo
    
    Muchas gracias

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById("scrollToTopButton").addEventListener("click", function(
   });
 });
 // Obtener la altura del navbar
-const navbarHeight = document.getElementById("navbar").offsetHeight;
+const navbarHeight = document.getElementById("navbarUp").offsetHeight;
 
 // Mostrar el botón cuando el usuario haya desplazado la altura del navbar
 window.onscroll = function() {
@@ -21,11 +21,11 @@ const cards = document.querySelectorAll('.card');
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    entry.target.classList.add('anim'); // Agregar clase de sombra cuando está en la vista
-                    entry.target.style.transform = 'scale(1.1)'; // Escalar al 110%
+                    entry.target.classList.add('agrandado'); // Agregar clase de sombra cuando está en la vista
+                    entry.target.classList.remove('achicado'); // Quitar clase de sombra cuando sale de la vista
                 } else {
-                    entry.target.classList.remove('anim'); // Quitar clase de sombra cuando sale de la vista
-                    entry.target.style.transform = 'scale(1)'; // Volver al tamaño original
+                    entry.target.classList.add('achicado'); // Agregar clase de sombra cuando está en la vista
+                    entry.target.classList.remove('agrandado'); // Quitar clase de sombra cuando sale de la vista
                 }
             });
         }, { threshold: 0.5 });
